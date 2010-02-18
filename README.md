dcd90ca600f98bb40915f68071912a6f3798a824
mcrittenden <mcrittenden@freshbox.(none)>
2010-02-18 17:08:26 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+MarkItUp for Chyrp
+===================
+
+Connects MarkItUp's Markdown editor to Chyrp's add/edit post interface.
+
+What is MarkItUp?
+------------------
+
+MarkItUp is a WYSIWYM (what you see is what you MEAN) editor that can handle multiple types of markup (BBCode, HTML, Markdown, Textile, etc). 
+
+What does this module do?
+--------------------------
+
+This module implements the Markdown set of MarkItUp and as such relies on the Markdown Chryp module. It automatically attaches itself to textareas on the add/edit post/page forms.
+
+How do I install it?
+----------------------
+
+Just move the top "markitup" folder into Chyrp's modules directory, and then enable it.
---
+++ markitup/markitup.php
@@ -0,0 +1,41 @@
+<?php
+
+class MarkItUp extends Modules {
+    function __init() {
+        $this->setPriority("admin_head", 8);
+    }
+
+    static function admin_head() {
+
+        global $admin;
+
+        // Is a post or page being edited? If not, end function now.
+        $action = isset(Route::current()->action) ? Route::current()->action : 'write_post';
+
+        $wysiwyg_actions = array(
+            'write_post',
+            'edit_post',
+            'write_page',
+            'edit_page'
+         );
+
+        if (!in_array($action, $wysiwyg_actions)) {
+          return;
+        }
+
+            $config = Config::current();
+            $path = $config->chyrp_url.'/modules/markitup/';
+
+        ?>
+
+        <script type="text/javascript" src="<?php echo $path; ?>markitup/jquery.markitup.pack.js" charset="utf-8"></script>
+        <script type="text/javascript" src="<?php echo $path; ?>markitup/sets/markdown/set.js" charset="utf-8"></script>
+        <script type="text/javascript" src="<?php echo $path; ?>markitup.js" charset="utf-8"></script>
+
+        <link href="<?php echo $path; ?>markitup/skins/simple/style.css" rel="stylesheet" type="text/css">
+        <link href="<?php echo $path; ?>markitup/sets/markdown/style.css" rel="stylesheet" type="text/css">
+
+        <?php
+    }
+}
+
